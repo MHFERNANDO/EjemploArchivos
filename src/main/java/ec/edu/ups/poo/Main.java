1c9eb59aaa4315f95abbc2a66e74009baa32c973
MHFERNANDO <fermarhug@gmail.com>
2025-07-11 12:44:00 -0500

    Actualizacion

--- src/main/java/ec/edu/ups/poo/Main.java
+++ src/main/java/ec/edu/ups/poo/Main.java
@@ -1,24 +1,35 @@
+/**
+ * Ejemplo.0
+ *Documentacion del Maven, son comentarios de documentacion para eso del html maven
+ *
+ * @Author Fernando Martinez
+ * @since 11 de junio de 2025
+ * @Version 1.0.2
+ *
+ * Tambien hay que documentar los constructores, todos los metodos tambien
+ * La primera linea simpre en relacion al codigo
+ */
+
 package ec.edu.ups.poo;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
-    public static void main (String args[]){
-        File raiz = new File("D:\\CP\\Universidad\\POO\\CarritoDeCompras");
-        File [] elementos = raiz.listFiles();
+    public static void main(String args[]) {
+        /*File raiz = new File("D:\\CP\\Universidad\\POO\\CarritoDeCompras");
+        File[] elementos = raiz.listFiles();
 
-        for(File elemento: elementos){
+        for (File elemento : elementos) {
             System.out.println(elemento.getAbsolutePath());
-            File [] subElementos = elemento.listFiles();
+            File[] subElementos = elemento.listFiles();
 
 
             try {
-                for (File subElemento : subElementos){
+                for (File subElemento : subElementos) {
                     System.out.println("--" + subElemento.getName());
                 }
-            } catch (NullPointerException e){
+            } catch (NullPointerException e) {
                 System.err.println("No contiene archivos ");
             }
 
@@ -33,26 +44,70 @@ public class Main {
             }
             File archivo = new File("hola.txt");
             archivo.createNewFile();
-        }catch (IOException ex) {
+        } catch (IOException ex) {
             System.err.println("Error de lectura/escritura ");
         }
 
-        try{
-            String ruta="c:/carpeta1/archivoTexto.txt";
+        try {
+            String ruta = "c:/carpeta1/archivoTexto.txt";
             FileWriter archivoEscritura = new FileWriter(ruta, true);
             BufferedWriter escritura = new BufferedWriter(archivoEscritura);
-            escritura.append("Hola mundo"+’\n’);
+            escritura.append("Hola mundo" + "\n");
             escritura.close();
             archivoEscritura.close();
-        } catch(FileNotFoundException e1){
+        } catch (FileNotFoundException e1) {
             System.out.println("Ruta de archivo no encontrada");
-        } catch(IOException e2) {
+        } catch (IOException e2) {
             System.out.println("Error de escritura");
-        } catch(Exception e3) {
+        } catch (Exception e3) {
+            System.out.println("Error General");
+        }*/
+        guardarAleatorio();
+        mostrarAleatorio();
+
+    }
+
+    public static void guardarAleatorio(){
+        try
+        {
+            String ruta = "C:\\Users\\ferma.LEGION\\OneDrive\\Desktop\\ruta\\archivoRandomico.dat";
+            RandomAccessFile archivoEscritura;
+            archivoEscritura = new RandomAccessFile(ruta,"rw");
+            archivoEscritura.writeInt(80);
+            archivoEscritura.writeDouble(100.2);
+            archivoEscritura.writeDouble(90.1);
+            archivoEscritura.writeInt(80);
+            archivoEscritura.close();
+        }
+        catch(FileNotFoundException e1){
+            System.out.println("Ruta de archivos no encontrada");
+        }catch(IOException e1){
+            System.out.println("Error de escritura");
+        }catch(Exception e1){
+            System.out.println("Error General");
+        }
+    }
+    public static void mostrarAleatorio(){
+        try
+        {
+            String ruta = "C:\\Users\\ferma.LEGION\\OneDrive\\Desktop\\ruta\\archivoRandomico.dat";
+            RandomAccessFile archivoLectura;
+            archivoLectura = new RandomAccessFile(ruta, "r");
+            archivoLectura.seek(12);
+            double r = archivoLectura.readDouble();
+            System.out.println(r);
+            archivoLectura.close();
+        }
+        catch (FileNotFoundException e1){
+            System.out.println("Ruta de archivo no encontrada");
+        }
+        catch(IOException e1){
+            System.out.println("Error de escritura");
+        }catch (Exception e1){
             System.out.println("Error General");
         }
     }
 
-    }
-
 }
+
+
